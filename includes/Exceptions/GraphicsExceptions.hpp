2e97214a562bfe0726383a7a3e66f9ad35675d56
razerx100 <saikatmandal8@gmail.com>
2021-09-18 19:02:19 +0600

    Added basic files and did Graphics Exception implementation

---
+++ includes/Exceptions/GraphicsExceptions.hpp
@@ -0,0 +1,45 @@
+#ifndef __GRAPHICS_EXCEPTIONS_HPP__
+#define __GRAPHICS_EXCEPTIONS_HPP__
+#include <Win32BaseException.hpp>
+#include <vector>
+
+class HrException : public Win32BaseException {
+public:
+	HrException(int line, const char* file, long hr) noexcept;
+	HrException(
+		int line, const char* file,
+		long hr, const std::vector<std::string>& infoMsgs
+	) noexcept;
+
+	void GenerateWhatBuffer() noexcept override;
+	const char* what() const noexcept override;
+	const char* GetType() const noexcept override;
+	std::string GetErrorInfo() const noexcept;
+
+private:
+	long m_hr;
+	std::string m_info;
+};
+
+class DeviceRemovedException : public HrException {
+public:
+	using HrException::HrException;
+
+	const char* GetType() const noexcept override;
+};
+
+class InfoException : public Exception {
+public:
+	InfoException(int line, const char* file,
+		const std::vector<std::string>& infoMsgs
+	) noexcept;
+
+	void GenerateWhatBuffer() noexcept override;
+	const char* what() const noexcept override;
+	const char* GetType() const noexcept override;
+	std::string GetErrorInfo() const noexcept;
+
+private:
+	std::string m_info;
+};
+#endif
---
+++ src/Exception/GraphicsExceptions.cpp
@@ -0,0 +1,88 @@
+#include <GraphicsExceptions.hpp>
+#include <sstream>
+
+// HR EXCEPTION
+HrException::HrException(int line, const char* file, long hr) noexcept
+	: Win32BaseException(line, file, hr) {
+	GenerateWhatBuffer();
+}
+
+void HrException::GenerateWhatBuffer() noexcept {
+	std::ostringstream oss;
+	oss << GetType() << "\n"
+		<< "[Error code] 0x" << std::hex << std::uppercase << GetErrorCode()
+		<< std::dec << " (" << static_cast<std::uint64_t>(GetErrorCode()) << ")\n\n"
+		<< "[Error String] " << GetErrorString() << "\n";
+	if (!m_info.empty())
+		oss << "[Error Info]\n" << GetErrorInfo() << "\n\n";
+	oss << GetOriginString();
+	m_whatBuffer = oss.str();
+}
+
+HrException::HrException(int line, const char* file, long hr,
+	const std::vector<std::string>& infoMsgs) noexcept
+	: Win32BaseException(line, file, hr) {
+	for (const std::string& m : infoMsgs) {
+		m_info += m;
+		m_info.append("\n");
+	}
+
+	if (!m_info.empty())
+		m_info.pop_back();
+
+	GenerateWhatBuffer();
+}
+
+const char* HrException::what() const noexcept {
+	return m_whatBuffer.c_str();
+}
+
+const char* HrException::GetType() const noexcept {
+	return "Graphics Exception";
+}
+
+std::string HrException::GetErrorInfo() const noexcept {
+	return m_info;
+}
+
+// DEVICE REMOVED EXCEPTION
+const char* DeviceRemovedException::GetType() const noexcept {
+	return "Graphics Exception [Device Removed]";
+}
+
+// INFO EXCEPTION
+InfoException::InfoException(int line, const char* file,
+	const std::vector<std::string>& infoMsgs) noexcept
+	: Exception(line, file) {
+
+	for (const std::string msg : infoMsgs) {
+		m_info += msg;
+		m_info.append("\n");
+	}
+
+	if (!m_info.empty())
+		m_info.pop_back();
+
+	GenerateWhatBuffer();
+}
+
+void InfoException::GenerateWhatBuffer() noexcept {
+	std::ostringstream oss;
+	oss << GetType() << "\n\n";
+	if (!m_info.empty())
+		oss << "[Error Info]\n" << GetErrorInfo() << "\n\n";
+	oss << GetOriginString();
+	m_whatBuffer = oss.str();
+}
+
+const char* InfoException::what() const noexcept {
+	return m_whatBuffer.c_str();
+}
+
+const char* InfoException::GetType() const noexcept {
+	return "Graphics Info Exception";
+}
+
+std::string InfoException::GetErrorInfo() const noexcept {
+	return m_info;
+}
