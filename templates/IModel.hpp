eb6a6aa11c84892065c0459148d055c78d09f7d7
razerx100 <saikatmandal8@gmail.com>
2021-11-29 20:01:14 +0600

    added updated IModel

--- templates/IModel.hpp
+++ templates/IModel.hpp
@@ -1,22 +1,23 @@
 #ifndef __I_MODEL_HPP__
 #define __I_MODEL_HPP__
 #include <cstdint>
-#include <DirectXMath.h>
+#include <CRSVector.hpp>
+#include <CRSMatrix.hpp>
 
 class IModel {
 public:
 	virtual ~IModel() = default;
 
-	virtual void SetSolidColor(const DirectX::XMVECTORF32& color) noexcept = 0;
+	virtual void SetSolidColor(const Ceres::VectorF32& colorVector) noexcept = 0;
 
 	virtual void GetVertices(
-		DirectX::XMFLOAT3* vertices, std::uint32_t& vertexCount
+		Ceres::Float32_3* vertices, std::uint32_t& vertexCount
 	) const noexcept = 0;
 	virtual void GetIndices(
 		std::uint16_t* indices, std::uint32_t& indexCount
 	) const noexcept = 0;
 
-	virtual DirectX::XMMATRIX GetTransform() const noexcept = 0;
-	virtual DirectX::XMVECTORF32 GetSolidColor() const noexcept = 0;
+	virtual void GetTransform(Ceres::Matrix& transform) const noexcept = 0;
+	virtual void GetSolidColor(Ceres::VectorF32& colorVector) const noexcept = 0;
 };
 #endif
