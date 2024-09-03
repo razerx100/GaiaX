cc7236eda4eed4e7c60afb46914824c90e1eebbb
razerx100 <saikatmandal8@gmail.com>
2024-09-03 18:58:02 +0100

    Move D3DDescriptorHeap

--- library/includes/D3D/D3DDescriptorHeapManager.hpp
+++ library/includes/D3D/D3DDescriptorHeapManager.hpp
@@ -123,4 +123,81 @@ public:
 		return *this;
 	}
 };
+
+class D3DDescriptorMap
+{
+public:
+	D3DDescriptorMap() : m_singleDescriptors{}, m_descriptorTables{} {}
+
+	D3DDescriptorMap& AddCBVGfx(UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress) noexcept;
+	D3DDescriptorMap& AddCBVCom(UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress) noexcept;
+
+	D3DDescriptorMap& AddUAVGfx(UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress) noexcept;
+	D3DDescriptorMap& AddUAVCom(UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress) noexcept;
+
+	D3DDescriptorMap& AddSRVGfx(UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress) noexcept;
+	D3DDescriptorMap& AddSRVCom(UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress) noexcept;
+
+	D3DDescriptorMap& AddDescTableGfx(UINT rootIndex, UINT descriptorIndex) noexcept;
+	D3DDescriptorMap& AddDescTableCom(UINT rootIndex, UINT descriptorIndex) noexcept;
+
+	void Bind(const D3DDescriptorHeap& descriptorHeap, ID3D12GraphicsCommandList* commandList) const;
+
+private:
+	template<void (ID3D12GraphicsCommandList::*bindViewFunction)(UINT, D3D12_GPU_VIRTUAL_ADDRESS)>
+	static void ProxyView(
+		ID3D12GraphicsCommandList* commandList, UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress
+	) {
+		(commandList->*bindViewFunction)(rootIndex, bufferAddress);
+	}
+	template<void (ID3D12GraphicsCommandList::*bindTableFunction)(UINT, D3D12_GPU_DESCRIPTOR_HANDLE)>
+	static void ProxyTable(
+		ID3D12GraphicsCommandList* commandList, UINT rootIndex, D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle
+	) {
+		(commandList->*bindTableFunction)(rootIndex, gpuHandle);
+	}
+
+	struct SingleDescriptorMap
+	{
+		UINT                      rootIndex;
+		D3D12_GPU_VIRTUAL_ADDRESS bufferAddress;
+		void(*bindViewFunction)(ID3D12GraphicsCommandList*, UINT, D3D12_GPU_VIRTUAL_ADDRESS);
+	};
+
+	struct DescriptorTableMap
+	{
+		UINT rootIndex;
+		UINT descriptorIndex;
+		void(*bindTableFunction)(ID3D12GraphicsCommandList*, UINT, D3D12_GPU_DESCRIPTOR_HANDLE);
+	};
+
+private:
+	std::vector<SingleDescriptorMap> m_singleDescriptors;
+	std::vector<DescriptorTableMap>  m_descriptorTables;
+
+public:
+	D3DDescriptorMap(const D3DDescriptorMap& other) noexcept
+		: m_singleDescriptors{ other.m_singleDescriptors },
+		m_descriptorTables{ other.m_descriptorTables }
+	{}
+	D3DDescriptorMap& operator=(const D3DDescriptorMap& other) noexcept
+	{
+		m_singleDescriptors = other.m_singleDescriptors;
+		m_descriptorTables  = other.m_descriptorTables;
+
+		return *this;
+	}
+
+	D3DDescriptorMap(D3DDescriptorMap&& other) noexcept
+		: m_singleDescriptors{ std::move(other.m_singleDescriptors) },
+		m_descriptorTables{ std::move(other.m_descriptorTables) }
+	{}
+	D3DDescriptorMap& operator=(D3DDescriptorMap&& other) noexcept
+	{
+		m_singleDescriptors = std::move(other.m_singleDescriptors);
+		m_descriptorTables  = std::move(other.m_descriptorTables);
+
+		return *this;
+	}
+};
 #endif
--- library/src/D3D/D3DDescriptorHeapManager.cpp
+++ library/src/D3D/D3DDescriptorHeapManager.cpp
@@ -197,3 +197,126 @@ void D3DReusableDescriptorHeap::ReserveNewElements(UINT newDescriptorCount)
     m_indicesManager.Resize(newDescriptorCount);
     m_descriptorHeap.Create(newDescriptorCount);
 }
+
+// D3D Descriptor Layout
+void D3DDescriptorMap::Bind(
+	const D3DDescriptorHeap& descriptorHeap, ID3D12GraphicsCommandList* commandList
+) const {
+	for (const auto& viewMap : m_singleDescriptors)
+		viewMap.bindViewFunction(commandList, viewMap.rootIndex, viewMap.bufferAddress);
+
+	for (const auto& tableMap : m_descriptorTables)
+		tableMap.bindTableFunction(
+			commandList, tableMap.rootIndex, descriptorHeap.GetGPUHandle(tableMap.descriptorIndex)
+		);
+}
+
+D3DDescriptorMap& D3DDescriptorMap::AddCBVGfx(
+	UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress
+) noexcept {
+	m_singleDescriptors.emplace_back(
+		SingleDescriptorMap{
+			.rootIndex        = rootIndex,
+			.bufferAddress    = bufferAddress,
+			.bindViewFunction = &ProxyView<&ID3D12GraphicsCommandList::SetGraphicsRootConstantBufferView>
+		}
+	);
+
+	return *this;
+}
+
+D3DDescriptorMap& D3DDescriptorMap::AddCBVCom(
+	UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress
+) noexcept {
+	m_singleDescriptors.emplace_back(
+		SingleDescriptorMap{
+			.rootIndex        = rootIndex,
+			.bufferAddress    = bufferAddress,
+			.bindViewFunction = &ProxyView<&ID3D12GraphicsCommandList::SetComputeRootConstantBufferView>
+		}
+	);
+
+	return *this;
+}
+
+D3DDescriptorMap& D3DDescriptorMap::AddUAVGfx(
+	UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress
+) noexcept {
+	m_singleDescriptors.emplace_back(
+		SingleDescriptorMap{
+			.rootIndex        = rootIndex,
+			.bufferAddress    = bufferAddress,
+			.bindViewFunction = &ProxyView<&ID3D12GraphicsCommandList::SetGraphicsRootUnorderedAccessView>
+		}
+	);
+
+	return *this;
+}
+
+D3DDescriptorMap& D3DDescriptorMap::AddUAVCom(
+	UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress
+) noexcept {
+	m_singleDescriptors.emplace_back(
+		SingleDescriptorMap{
+			.rootIndex        = rootIndex,
+			.bufferAddress    = bufferAddress,
+			.bindViewFunction = &ProxyView<&ID3D12GraphicsCommandList::SetComputeRootUnorderedAccessView>
+		}
+	);
+
+	return *this;
+}
+
+D3DDescriptorMap& D3DDescriptorMap::AddSRVGfx(
+	UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress
+) noexcept {
+	m_singleDescriptors.emplace_back(
+		SingleDescriptorMap{
+			.rootIndex        = rootIndex,
+			.bufferAddress    = bufferAddress,
+			.bindViewFunction = &ProxyView<&ID3D12GraphicsCommandList::SetGraphicsRootShaderResourceView>
+		}
+	);
+
+	return *this;
+}
+
+D3DDescriptorMap& D3DDescriptorMap::AddSRVCom(
+	UINT rootIndex, D3D12_GPU_VIRTUAL_ADDRESS bufferAddress
+) noexcept {
+	m_singleDescriptors.emplace_back(
+		SingleDescriptorMap{
+			.rootIndex        = rootIndex,
+			.bufferAddress    = bufferAddress,
+			.bindViewFunction = &ProxyView<&ID3D12GraphicsCommandList::SetComputeRootShaderResourceView>
+		}
+	);
+
+	return *this;
+}
+
+D3DDescriptorMap& D3DDescriptorMap::AddDescTableGfx(UINT rootIndex, UINT descriptorIndex) noexcept
+{
+	m_descriptorTables.emplace_back(
+		DescriptorTableMap{
+			.rootIndex         = rootIndex,
+			.descriptorIndex   = descriptorIndex,
+			.bindTableFunction = &ProxyTable<&ID3D12GraphicsCommandList::SetGraphicsRootDescriptorTable>
+		}
+	);
+
+	return *this;
+}
+
+D3DDescriptorMap& D3DDescriptorMap::AddDescTableCom(UINT rootIndex, UINT descriptorIndex) noexcept
+{
+	m_descriptorTables.emplace_back(
+		DescriptorTableMap{
+			.rootIndex         = rootIndex,
+			.descriptorIndex   = descriptorIndex,
+			.bindTableFunction = &ProxyTable<&ID3D12GraphicsCommandList::SetComputeRootDescriptorTable>
+		}
+	);
+
+	return *this;
+}
