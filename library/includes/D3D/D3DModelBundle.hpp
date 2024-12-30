82eed67ada0636a3b88d0f145d403020e27468c3
razerx100 <saikatmandal8@gmail.com>
2024-12-30 15:50:47 +0000

    Separate ModelBundle

---
+++ library/includes/D3D/D3DModelBundle.hpp
@@ -0,0 +1,348 @@
+#ifndef D3D_MODEL_BUNDLE_HPP_
+#define D3D_MODEL_BUNDLE_HPP_
+#include <memory>
+#include <vector>
+#include <Model.hpp>
+#include <MeshManagerMeshShader.hpp>
+#include <MeshManagerVertexShader.hpp>
+
+class ModelBundleBase
+{
+public:
+	ModelBundleBase() : m_psoIndex{ 0u } {}
+
+	void SetPSOIndex(std::uint32_t index) noexcept { m_psoIndex = index; }
+
+	[[nodiscard]]
+	std::uint32_t GetPSOIndex() const noexcept { return m_psoIndex; }
+
+	[[nodiscard]]
+	static D3D12_DRAW_INDEXED_ARGUMENTS GetDrawIndexedIndirectCommand(
+		const MeshTemporaryDetailsVS& meshDetailsVS
+	) noexcept;
+
+protected:
+	std::uint32_t m_psoIndex;
+
+public:
+	ModelBundleBase(const ModelBundleBase&) = delete;
+	ModelBundleBase& operator=(const ModelBundleBase&) = delete;
+
+	ModelBundleBase(ModelBundleBase&& other) noexcept
+		: m_psoIndex{ other.m_psoIndex }
+	{}
+	ModelBundleBase& operator=(ModelBundleBase&& other) noexcept
+	{
+		m_psoIndex = other.m_psoIndex;
+
+		return *this;
+	}
+};
+
+class ModelBundleVSIndividual : public ModelBundleBase
+{
+public:
+	ModelBundleVSIndividual() : ModelBundleBase{}, m_modelBufferIndices{}, m_modelBundle{} {}
+
+	void SetModelBundle(
+		std::shared_ptr<ModelBundle> bundle, std::vector<std::uint32_t> modelBufferIndices
+	) noexcept;
+	void Draw(
+		const D3DCommandList& graphicsList, UINT constantsRootIndex,
+		const MeshManagerVertexShader& meshBundle
+	) const noexcept;
+
+	[[nodiscard]]
+	static consteval UINT GetConstantCount() noexcept { return 1u; }
+
+	[[nodiscard]]
+	std::uint32_t GetMeshBundleIndex() const noexcept { return m_modelBundle->GetMeshBundleIndex(); }
+	[[nodiscard]]
+	const std::vector<std::uint32_t>& GetIndices() const noexcept { return m_modelBufferIndices; }
+
+	[[nodiscard]]
+	std::uint32_t GetID() const noexcept
+	{
+		if (!std::empty(m_modelBufferIndices))
+			return m_modelBufferIndices.front();
+		else
+			return std::numeric_limits<std::uint32_t>::max();
+	}
+
+private:
+	std::vector<std::uint32_t>   m_modelBufferIndices;
+	std::shared_ptr<ModelBundle> m_modelBundle;
+
+public:
+	ModelBundleVSIndividual(const ModelBundleVSIndividual&) = delete;
+	ModelBundleVSIndividual& operator=(const ModelBundleVSIndividual&) = delete;
+
+	ModelBundleVSIndividual(ModelBundleVSIndividual&& other) noexcept
+		: ModelBundleBase{ std::move(other) },
+		m_modelBufferIndices{ std::move(other.m_modelBufferIndices) },
+		m_modelBundle{ std::move(other.m_modelBundle) }
+	{}
+	ModelBundleVSIndividual& operator=(ModelBundleVSIndividual&& other) noexcept
+	{
+		ModelBundleBase::operator=(std::move(other));
+		m_modelBufferIndices     = std::move(other.m_modelBufferIndices);
+		m_modelBundle            = std::move(other.m_modelBundle);
+
+		return *this;
+	}
+};
+
+class ModelBundleMSIndividual : public ModelBundleBase
+{
+public:
+	// In D3D12, root constants/CBV are laid out as vec4, so we need
+	// to either align to 16bytes or pass this data at the end and gonna do that.
+	struct MeshDetails
+	{
+		std::uint32_t meshletCount;
+		std::uint32_t meshletOffset;
+		std::uint32_t indexOffset;
+		std::uint32_t primOffset;
+		std::uint32_t vertexOffset;
+	};
+	struct ModelDetailsMS
+	{
+		MeshDetails   meshDetails;
+		std::uint32_t modelBufferIndex;
+	};
+
+public:
+	ModelBundleMSIndividual() : ModelBundleBase{}, m_modelBundle{}, m_modelBufferIndices{} {}
+
+	void SetModelBundle(
+		std::shared_ptr<ModelBundle> bundle, std::vector<std::uint32_t> modelBufferIndices
+	) noexcept;
+
+	void Draw(
+		const D3DCommandList& graphicsList, UINT constantsRootIndex,
+		const MeshManagerMeshShader& meshBundle
+	) const noexcept;
+
+	[[nodiscard]]
+	static consteval UINT GetConstantCount() noexcept
+	{
+		return static_cast<UINT>(sizeof(ModelDetailsMS) / sizeof(UINT));
+	}
+
+	[[nodiscard]]
+	const std::vector<std::uint32_t>& GetIndices() const noexcept { return m_modelBufferIndices; }
+	[[nodiscard]]
+	std::uint32_t GetMeshBundleIndex() const noexcept { return m_modelBundle->GetMeshBundleIndex(); }
+
+	[[nodiscard]]
+	std::uint32_t GetID() const noexcept
+	{
+		if (!std::empty(m_modelBufferIndices))
+			return m_modelBufferIndices.front();
+		else
+			return std::numeric_limits<std::uint32_t>::max();
+	}
+
+private:
+	[[nodiscard]]
+	static UINT DivRoundUp(UINT num, UINT den) noexcept
+	{
+		return (num + den - 1) / den;
+	}
+
+private:
+	std::shared_ptr<ModelBundle> m_modelBundle;
+	std::vector<std::uint32_t>   m_modelBufferIndices;
+
+	static constexpr UINT s_amplificationLaneCount = 32u;
+
+public:
+	ModelBundleMSIndividual(const ModelBundleMSIndividual&) = delete;
+	ModelBundleMSIndividual& operator=(const ModelBundleMSIndividual&) = delete;
+
+	ModelBundleMSIndividual(ModelBundleMSIndividual&& other) noexcept
+		: ModelBundleBase{ std::move(other) },
+		m_modelBundle{ std::move(other.m_modelBundle) },
+		m_modelBufferIndices{ std::move(other.m_modelBufferIndices) }
+	{}
+	ModelBundleMSIndividual& operator=(ModelBundleMSIndividual&& other) noexcept
+	{
+		ModelBundleBase::operator=(std::move(other));
+		m_modelBundle            = std::move(other.m_modelBundle);
+		m_modelBufferIndices     = std::move(other.m_modelBufferIndices);
+
+		return *this;
+	}
+};
+
+class ModelBundleCSIndirect
+{
+public:
+	struct CullingData
+	{
+		std::uint32_t commandCount;
+		std::uint32_t commandOffset;// Next Vec2 starts at 8bytes offset
+	};
+
+	struct IndirectArgument
+	{
+		UINT                         modelIndex;
+		D3D12_DRAW_INDEXED_ARGUMENTS drawArguments;
+	};
+
+public:
+	ModelBundleCSIndirect();
+
+	void SetModelBundle(std::shared_ptr<ModelBundle> bundle) noexcept;
+	void CreateBuffers(
+		StagingBufferManager& stagingBufferMan,
+		std::vector<SharedBufferCPU>& argumentInputSharedBuffer,
+		SharedBufferCPU& cullingSharedBuffer, SharedBufferGPU& perModelDataSharedBuffer,
+		std::vector<std::uint32_t> modelIndices, TemporaryDataBufferGPU& tempBuffer
+	);
+
+	void Update(size_t bufferIndex, const MeshManagerVertexShader& meshBundle) const noexcept;
+
+	void ResetCullingData() const noexcept;
+
+	[[nodiscard]]
+	std::uint32_t GetMeshBundleIndex() const noexcept { return m_modelBundle->GetMeshBundleIndex(); }
+
+	[[nodiscard]]
+	std::uint32_t GetID() const noexcept
+	{
+		if (!std::empty(m_modelIndices))
+			return m_modelIndices.front();
+		else
+			return std::numeric_limits<std::uint32_t>::max();
+	}
+	[[nodiscard]]
+	// Must be called after the buffers have been created.
+	std::uint32_t GetModelBundleIndex() const noexcept
+	{
+		return static_cast<std::uint32_t>(m_cullingSharedData.offset / sizeof(CullingData));
+	}
+	[[nodiscard]]
+	UINT GetModelCount() const noexcept
+	{
+		return static_cast<UINT>(std::size(m_modelIndices));
+	}
+	[[nodiscard]]
+	const std::vector<std::uint32_t>& GetModelIndices() const noexcept { return m_modelIndices; }
+
+	[[nodiscard]]
+	const std::vector<SharedBufferData>& GetArgumentInputSharedData() const noexcept
+	{
+		return m_argumentInputSharedData;
+	}
+	[[nodiscard]]
+	const SharedBufferData& GetCullingSharedData() const noexcept { return m_cullingSharedData; }
+	[[nodiscard]]
+	const SharedBufferData& GetPerModelDataSharedData() const noexcept
+	{
+		return m_perModelSharedData;
+	}
+
+private:
+	SharedBufferData              m_perModelSharedData;
+	SharedBufferData              m_cullingSharedData;
+	std::vector<SharedBufferData> m_argumentInputSharedData;
+	std::vector<std::uint32_t>    m_modelIndices;
+	std::shared_ptr<ModelBundle>  m_modelBundle;
+
+public:
+	ModelBundleCSIndirect(const ModelBundleCSIndirect&) = delete;
+	ModelBundleCSIndirect& operator=(const ModelBundleCSIndirect&) = delete;
+
+	ModelBundleCSIndirect(ModelBundleCSIndirect&& other) noexcept
+		: m_perModelSharedData{ other.m_perModelSharedData },
+		m_cullingSharedData{ other.m_cullingSharedData },
+		m_argumentInputSharedData{ std::move(other.m_argumentInputSharedData) },
+		m_modelIndices{ std::move(other.m_modelIndices) },
+		m_modelBundle{ std::move(other.m_modelBundle) }
+	{}
+	ModelBundleCSIndirect& operator=(ModelBundleCSIndirect&& other) noexcept
+	{
+		m_perModelSharedData      = other.m_perModelSharedData;
+		m_cullingSharedData       = other.m_cullingSharedData;
+		m_argumentInputSharedData = std::move(other.m_argumentInputSharedData);
+		m_modelIndices            = std::move(other.m_modelIndices);
+		m_modelBundle             = std::move(other.m_modelBundle);
+
+		return *this;
+	}
+};
+
+class ModelBundleVSIndirect : public ModelBundleBase
+{
+public:
+	ModelBundleVSIndirect();
+
+	void SetModelBundle(std::shared_ptr<ModelBundle> bundle) noexcept;
+
+	void CreateBuffers(
+		std::vector<SharedBufferGPUWriteOnly>& argumentOutputSharedBuffers,
+		std::vector<SharedBufferGPUWriteOnly>& counterSharedBuffers, UINT modelCount
+	);
+	void Draw(
+		size_t frameIndex, ID3D12CommandSignature* commandSignature, const D3DCommandList& graphicsList
+	) const noexcept;
+
+	void SetID(std::uint32_t bundleID) noexcept { m_bundleID = bundleID; }
+	[[nodiscard]]
+	std::uint32_t GetID() const noexcept { return m_bundleID; }
+
+	[[nodiscard]]
+	std::uint32_t GetMeshBundleIndex() const noexcept { return m_modelBundle->GetMeshBundleIndex(); }
+	[[nodiscard]]
+	const std::vector<SharedBufferData>& GetArgumentOutputSharedData() const noexcept
+	{
+		return m_argumentOutputSharedData;
+	}
+	[[nodiscard]]
+	const std::vector<SharedBufferData>& GetCounterSharedData() const noexcept
+	{
+		return m_counterSharedData;
+	}
+
+	[[nodiscard]]
+	static UINT64 GetCounterBufferSize() noexcept { return s_counterBufferSize; }
+
+	[[nodiscard]]
+	static constexpr UINT GetConstantCount() noexcept { return 1u; }
+
+private:
+	UINT                          m_modelCount;
+	std::shared_ptr<ModelBundle>  m_modelBundle;
+	std::vector<SharedBufferData> m_argumentOutputSharedData;
+	std::vector<SharedBufferData> m_counterSharedData;
+	std::uint32_t                 m_bundleID;
+
+	inline static UINT64 s_counterBufferSize = static_cast<UINT64>(sizeof(std::uint32_t));
+
+public:
+	ModelBundleVSIndirect(const ModelBundleVSIndirect&) = delete;
+	ModelBundleVSIndirect& operator=(const ModelBundleVSIndirect&) = delete;
+
+	ModelBundleVSIndirect(ModelBundleVSIndirect&& other) noexcept
+		: ModelBundleBase{ std::move(other) },
+		m_modelCount{ other.m_modelCount },
+		m_modelBundle{ std::move(other.m_modelBundle) },
+		m_argumentOutputSharedData{ std::move(other.m_argumentOutputSharedData) },
+		m_counterSharedData{ std::move(other.m_counterSharedData) },
+		m_bundleID{ other.m_bundleID }
+	{}
+	ModelBundleVSIndirect& operator=(ModelBundleVSIndirect&& other) noexcept
+	{
+		ModelBundleBase::operator=(std::move(other));
+		m_modelCount               = other.m_modelCount;
+		m_modelBundle              = std::move(other.m_modelBundle);
+		m_argumentOutputSharedData = std::move(other.m_argumentOutputSharedData);
+		m_counterSharedData        = std::move(other.m_counterSharedData);
+		m_bundleID                 = other.m_bundleID;
+
+		return *this;
+	}
+};
+
+#endif
---
+++ library/src/D3D/D3DModelBundle.cpp
@@ -0,0 +1,304 @@
+#include <D3DModelBundle.hpp>
+#include <VectorToSharedPtr.hpp>
+
+// Model Bundle
+D3D12_DRAW_INDEXED_ARGUMENTS ModelBundleBase::GetDrawIndexedIndirectCommand(
+	const MeshTemporaryDetailsVS& meshDetailsVS
+) noexcept {
+	const D3D12_DRAW_INDEXED_ARGUMENTS indirectCommand
+	{
+		.IndexCountPerInstance = meshDetailsVS.indexCount,
+		.InstanceCount         = 1u,
+		.StartIndexLocation    = meshDetailsVS.indexOffset,
+		.BaseVertexLocation    = 0,
+		.StartInstanceLocation = 0u
+	};
+
+	return indirectCommand;
+}
+
+// Model Bundle VS Individual
+void ModelBundleVSIndividual::SetModelBundle(
+	std::shared_ptr<ModelBundle> bundle, std::vector<std::uint32_t> modelBufferIndices
+) noexcept {
+	m_modelBufferIndices = std::move(modelBufferIndices);
+	m_modelBundle        = std::move(bundle);
+}
+
+void ModelBundleVSIndividual::Draw(
+	const D3DCommandList& graphicsList, UINT constantsRootIndex,
+	const MeshManagerVertexShader& meshBundle
+) const noexcept {
+	ID3D12GraphicsCommandList* cmdList = graphicsList.Get();
+	const auto& models                 = m_modelBundle->GetModels();
+
+	for (size_t index = 0; index < std::size(models); ++index)
+	{
+		constexpr UINT pushConstantCount = GetConstantCount();
+
+		cmdList->SetGraphicsRoot32BitConstants(
+			constantsRootIndex, pushConstantCount, &m_modelBufferIndices[index], 0u
+		);
+
+		const std::shared_ptr<Model>& model = models[index];
+
+		const D3D12_DRAW_INDEXED_ARGUMENTS meshArgs = GetDrawIndexedIndirectCommand(
+			meshBundle.GetMeshDetails(model->GetMeshIndex())
+		);
+
+		cmdList->DrawIndexedInstanced(
+			meshArgs.IndexCountPerInstance, meshArgs.InstanceCount, meshArgs.StartIndexLocation,
+			meshArgs.BaseVertexLocation, meshArgs.StartInstanceLocation
+		);
+	}
+}
+
+// Model Bundle MS
+void ModelBundleMSIndividual::SetModelBundle(
+	std::shared_ptr<ModelBundle> bundle, std::vector<std::uint32_t> modelBufferIndices
+) noexcept {
+	m_modelBundle        = std::move(bundle);
+	m_modelBufferIndices = std::move(modelBufferIndices);
+}
+
+void ModelBundleMSIndividual::Draw(
+	const D3DCommandList& graphicsList, UINT constantsRootIndex, const MeshManagerMeshShader& meshBundle
+) const noexcept {
+	ID3D12GraphicsCommandList6* cmdList = graphicsList.Get();
+	const auto& models                  = m_modelBundle->GetModels();
+
+	for (size_t index = 0u; index < std::size(models); ++index)
+	{
+		const auto& model = models[index];
+
+		constexpr UINT pushConstantCount = GetConstantCount();
+		constexpr UINT constBufferOffset = MeshManagerMeshShader::GetConstantCount();
+
+		const MeshTemporaryDetailsMS& meshDetailsMS = meshBundle.GetMeshDetails(model->GetMeshIndex());
+
+		const ModelDetailsMS constants
+		{
+			.meshDetails      = MeshDetails
+				{
+					.meshletCount  = meshDetailsMS.meshletCount,
+					.meshletOffset = meshDetailsMS.meshletOffset,
+					.indexOffset   = meshDetailsMS.indexOffset,
+					.primOffset    = meshDetailsMS.primitiveOffset,
+					.vertexOffset  = meshDetailsMS.vertexOffset,
+				},
+			.modelBufferIndex = m_modelBufferIndices[index]
+		};
+
+		cmdList->SetGraphicsRoot32BitConstants(
+			constantsRootIndex, pushConstantCount, &constants, constBufferOffset
+		);
+
+		// If we have an Amplification shader, this will launch an amplification global workGroup.
+		// We would want each Amplification shader lane to process a meshlet and launch the necessary
+		// Mesh Shader workGroups. On Nvdia we can have a maximum of 32 lanes active
+		// in a wave and 64 on AMD. So, a workGroup will be able to work on 32/64
+		// meshlets concurrently.
+		const UINT amplficationGroupCount = DivRoundUp(
+			meshDetailsMS.meshletCount, s_amplificationLaneCount
+		);
+
+		cmdList->DispatchMesh(amplficationGroupCount, 1u, 1u);
+		// It might be worth checking if we are reaching the Group Count Limit and if needed
+		// launch more Groups. Could achieve that by passing a GroupLaunch index.
+	}
+}
+
+// Model Bundle VS Indirect
+ModelBundleVSIndirect::ModelBundleVSIndirect()
+	: ModelBundleBase{}, m_modelCount{ 0u }, m_modelBundle{}, m_argumentOutputSharedData{},
+	m_counterSharedData{}, m_bundleID{ 0u }
+{}
+
+void ModelBundleVSIndirect::SetModelBundle(std::shared_ptr<ModelBundle> bundle) noexcept
+{
+	m_modelBundle = std::move(bundle);
+}
+
+void ModelBundleVSIndirect::CreateBuffers(
+	std::vector<SharedBufferGPUWriteOnly>& argumentOutputSharedBuffers,
+	std::vector<SharedBufferGPUWriteOnly>& counterSharedBuffers, UINT modelCount
+) {
+	constexpr size_t argStrideSize      = sizeof(ModelBundleCSIndirect::IndirectArgument);
+	const auto argumentOutputBufferSize = static_cast<UINT64>(modelCount * argStrideSize);
+	m_modelCount                        = modelCount;
+
+	{
+		const size_t argumentOutputBufferCount = std::size(argumentOutputSharedBuffers);
+		m_argumentOutputSharedData.resize(argumentOutputBufferCount);
+
+		for (size_t index = 0u; index < argumentOutputBufferCount; ++index)
+		{
+			SharedBufferData& argumentOutputSharedData = m_argumentOutputSharedData[index];
+
+			argumentOutputSharedData = argumentOutputSharedBuffers[index].AllocateAndGetSharedData(
+				argumentOutputBufferSize
+			);
+		}
+	}
+
+	{
+		const size_t counterBufferCount = std::size(counterSharedBuffers);
+		m_counterSharedData.resize(counterBufferCount);
+
+		for (size_t index = 0u; index < counterBufferCount; ++index)
+			m_counterSharedData[index] = counterSharedBuffers[index].AllocateAndGetSharedData(
+				s_counterBufferSize
+			);
+	}
+}
+
+void ModelBundleVSIndirect::Draw(
+	size_t frameIndex, ID3D12CommandSignature* commandSignature, const D3DCommandList& graphicsList
+) const noexcept {
+	ID3D12GraphicsCommandList6* cmdList = graphicsList.Get();
+
+	const SharedBufferData& argumentOutputSharedData = m_argumentOutputSharedData[frameIndex];
+	const SharedBufferData& counterSharedData        = m_counterSharedData[frameIndex];
+
+	cmdList->ExecuteIndirect(
+		commandSignature, m_modelCount,
+		argumentOutputSharedData.bufferData->Get(), argumentOutputSharedData.offset,
+		counterSharedData.bufferData->Get(), counterSharedData.offset
+	);
+}
+
+// Model Bundle CS Indirect
+ModelBundleCSIndirect::ModelBundleCSIndirect()
+	: m_perModelSharedData{ nullptr, 0u, 0u }, m_cullingSharedData{ nullptr, 0u, 0u },
+	m_argumentInputSharedData{}, m_modelBundle{}, m_modelIndices{}
+{}
+
+void ModelBundleCSIndirect::SetModelBundle(std::shared_ptr<ModelBundle> bundle) noexcept
+{
+	m_modelBundle = std::move(bundle);
+}
+
+void ModelBundleCSIndirect::ResetCullingData() const noexcept
+{
+	// Before destroying an object the command count needs to be set to 0,
+	// so the model isn't processed in the compute shader anymore.
+	std::uint32_t commandCount  = 0u;
+
+	Buffer const* cullingBuffer = m_cullingSharedData.bufferData;
+
+	memcpy(
+		cullingBuffer->CPUHandle() + m_cullingSharedData.offset,
+		&commandCount, sizeof(std::uint32_t)
+	);
+}
+
+void ModelBundleCSIndirect::CreateBuffers(
+	StagingBufferManager& stagingBufferMan,
+	std::vector<SharedBufferCPU>& argumentInputSharedBuffer,
+	SharedBufferCPU& cullingSharedBuffer, SharedBufferGPU& perModelDataSharedBuffer,
+	std::vector<std::uint32_t> modelIndices, TemporaryDataBufferGPU& tempBuffer
+) {
+	constexpr size_t argumentStrideSize = sizeof(IndirectArgument);
+
+	const auto argumentCount      = static_cast<std::uint32_t>(std::size(m_modelBundle->GetModels()));
+	const auto argumentBufferSize = static_cast<UINT64>(argumentStrideSize * argumentCount);
+	const auto cullingDataSize    = static_cast<UINT64>(sizeof(CullingData));
+	const auto perModelDataSize   = static_cast<UINT64>(sizeof(std::uint32_t) * argumentCount);
+
+	{
+		const size_t argumentInputBufferCount = std::size(argumentInputSharedBuffer);
+		m_argumentInputSharedData.resize(argumentInputBufferCount);
+
+		for (size_t index = 0u; index < argumentInputBufferCount; ++index)
+			m_argumentInputSharedData[index] = argumentInputSharedBuffer[index].AllocateAndGetSharedData(
+				argumentBufferSize
+			);
+	}
+
+	// Set the culling data.
+	{
+		m_cullingSharedData = cullingSharedBuffer.AllocateAndGetSharedData(
+			cullingDataSize, true
+		);
+
+		CullingData cullingData
+		{
+			.commandCount  = argumentCount,
+			.commandOffset = 0u
+		};
+
+		if (!std::empty(m_argumentInputSharedData))
+		{
+			const SharedBufferData& sharedBufferData = m_argumentInputSharedData.front();
+
+			// The offset on each sharedBuffer should be the same.
+			cullingData.commandOffset = static_cast<std::uint32_t>(
+				sharedBufferData.offset / argumentStrideSize
+			);
+		}
+
+		Buffer const* cullingBuffer = m_cullingSharedData.bufferData;
+
+		memcpy(
+			cullingBuffer->CPUHandle() + m_cullingSharedData.offset,
+			&cullingData, cullingDataSize
+		);
+	}
+
+	m_perModelSharedData = perModelDataSharedBuffer.AllocateAndGetSharedData(
+		perModelDataSize, tempBuffer
+	);
+
+	const auto modelBundleIndex = GetModelBundleIndex();
+
+	// Each thread will process a single model independently. And since we are trying to
+	// cull all of the models across all of the bundles with a single call to dispatch, we can't
+	// set the index as constantData per bundle. So, we will be giving each model the index
+	// of its bundle so each thread can work independently.
+	auto modelBundleIndicesInModels = std::vector<std::uint32_t>(argumentCount, modelBundleIndex);
+
+	std::shared_ptr<std::uint8_t[]> perModelTempData = CopyVectorToSharedPtr(
+		modelBundleIndicesInModels
+	);
+
+	stagingBufferMan.AddBuffer(
+		std::move(perModelTempData), perModelDataSize,
+		m_perModelSharedData.bufferData, m_perModelSharedData.offset,
+		tempBuffer
+	);
+
+	m_modelIndices = std::move(modelIndices);
+}
+
+void ModelBundleCSIndirect::Update(
+	size_t bufferIndex, const MeshManagerVertexShader& meshBundle
+) const noexcept {
+	const SharedBufferData& argumentInputSharedData = m_argumentInputSharedData[bufferIndex];
+
+	std::uint8_t* argumentInputStart
+		= argumentInputSharedData.bufferData->CPUHandle() + argumentInputSharedData.offset;
+
+	constexpr size_t argumentStride = sizeof(IndirectArgument);
+	size_t modelOffset              = 0u;
+	const auto& models              = m_modelBundle->GetModels();
+
+	const size_t modelCount         = std::size(models);
+
+	for (size_t index = 0u; index < modelCount; ++index)
+	{
+		const std::shared_ptr<Model>& model = models[index];
+		const UINT modelIndex               = m_modelIndices[index];
+
+		IndirectArgument arguments
+		{
+			.modelIndex    = modelIndex,
+			.drawArguments = ModelBundleBase::GetDrawIndexedIndirectCommand(
+				meshBundle.GetMeshDetails(model->GetMeshIndex())
+			)
+		};
+
+		memcpy(argumentInputStart + modelOffset, &arguments, argumentStride);
+
+		modelOffset += argumentStride;
+	}
+}
