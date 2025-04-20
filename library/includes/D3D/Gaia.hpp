0ce619aaefe729e4814c3476f09bea6b7c7a3f87
razerx100 <saikatmandal8@gmail.com>
2025-04-20 12:22:52 +0100

    Make Gaia and RendererDx12 inline

--- library/includes/D3D/Gaia.hpp
+++ library/includes/D3D/Gaia.hpp
@@ -3,7 +3,8 @@
 #include <DeviceManager.hpp>
 #include <SwapchainManager.hpp>
 #include <RendererTypes.hpp>
-#include <RenderEngine.hpp>
+#include <RenderEngineVS.hpp>
+#include <RenderEngineMS.hpp>
 
 class Gaia
 {
@@ -11,12 +12,49 @@ public:
 	Gaia(
 		void* windowHandle, UINT width, UINT height, std::uint32_t bufferCount,
 		std::shared_ptr<ThreadPool> threadPool, RenderEngineType engineType
-	);
+	) : m_deviceManager{}, m_rtvHeap{}, m_renderEngine{}, m_swapchain{}, m_windowWidth{ 0u },
+		m_windowHeight{ 0u }
+	{
+		CreateDevice();
 
-	void FinaliseInitialisation();
+		CreateRenderEngine(engineType, std::move(threadPool), bufferCount);
 
-	void Resize(UINT width, UINT height);
-	void Render();
+		CreateSwapchain(bufferCount, windowHandle);
+
+		Resize(width, height);
+	}
+
+	void FinaliseInitialisation()
+	{
+		m_renderEngine->FinaliseInitialisation(m_deviceManager);
+	}
+
+	void Resize(UINT width, UINT height)
+	{
+		// Only recreate these if the new resolution is different.
+		if (m_windowWidth != width || m_windowHeight != height)
+		{
+			m_renderEngine->WaitForGPUToFinish();
+
+			// Must resize the swapchain first.
+			m_swapchain->Resize(width, height);
+
+			m_renderEngine->Resize(width, height);
+
+			m_windowWidth  = width;
+			m_windowHeight = height;
+		}
+	}
+
+	void Render()
+	{
+		const size_t nextImageIndex      = m_swapchain->GetCurrentBackBufferIndex();
+		ID3D12Resource* nextRenderTarget = m_swapchain->GetRenderTarget(nextImageIndex);
+
+		m_renderEngine->Render(nextImageIndex, nextRenderTarget);
+
+		m_swapchain->Present();
+	}
 
 	// Instead of creating useless wrapper functions, let's just return a reference to the RenderEngine.
 	[[nodiscard]]
@@ -31,22 +69,67 @@ public:
 	}
 
 	[[nodiscard]]
-	std::uint32_t AddExternalRenderPass();
+	std::uint32_t AddExternalRenderPass()
+	{
+		return m_renderEngine->AddExternalRenderPass(m_rtvHeap.get());
+	}
 
-	void SetSwapchainExternalRenderPass();
+	void SetSwapchainExternalRenderPass()
+	{
+		m_renderEngine->SetSwapchainExternalRenderPass(m_rtvHeap.get());
+	}
 
 	[[nodiscard]]
-	DeviceManager::Resolution GetFirstDisplayCoordinates() const;
+	DeviceManager::Resolution GetFirstDisplayCoordinates() const
+	{
+		return m_deviceManager.GetDisplayResolution(0u);
+	}
 
 	[[nodiscard]]
-	SwapchainManager::Extent GetCurrentRenderArea() const noexcept;
+	SwapchainManager::Extent GetCurrentRenderArea() const noexcept
+	{
+		return m_swapchain->GetCurrentRenderArea();
+	}
 
 private:
-	void CreateDevice();
+	void CreateDevice()
+	{
+#ifndef NDEBUG
+		m_deviceManager.GetDebugLogger().AddCallbackType(DebugCallbackType::FileOut);
+#endif
+		m_deviceManager.Create(s_featureLevel);
+
+		m_rtvHeap = std::make_unique<D3DReusableDescriptorHeap>(
+			m_deviceManager.GetDevice(), D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
+			D3D12_DESCRIPTOR_HEAP_FLAG_NONE
+		);
+	}
+
 	void CreateRenderEngine(
-		RenderEngineType engineType, std::shared_ptr<ThreadPool>&& threadPool, std::uint32_t frameCount
-	);
-	void CreateSwapchain(std::uint32_t frameCount, void* windowHandle);
+		RenderEngineType engineType, std::shared_ptr<ThreadPool>&& threadPool,
+		std::uint32_t frameCount
+	) {
+		if (engineType == RenderEngineType::IndividualDraw)
+			m_renderEngine = std::make_unique<RenderEngineVSIndividual>(
+				m_deviceManager, std::move(threadPool), frameCount
+			);
+		else if (engineType == RenderEngineType::IndirectDraw)
+			m_renderEngine = std::make_unique<RenderEngineVSIndirect>(
+				m_deviceManager, std::move(threadPool), frameCount
+			);
+		else if (engineType == RenderEngineType::MeshDraw)
+			m_renderEngine = std::make_unique<RenderEngineMS>(
+				m_deviceManager, std::move(threadPool), frameCount
+			);
+	}
+
+	void CreateSwapchain(std::uint32_t frameCount, void* windowHandle)
+	{
+		m_swapchain = std::make_unique<SwapchainManager>(
+			m_rtvHeap.get(), frameCount, m_deviceManager.GetFactory(),
+			m_renderEngine->GetPresentQueue(), static_cast<HWND>(windowHandle)
+		);
+	}
 
 private:
 	DeviceManager                              m_deviceManager;
--- library/includes/D3D/RendererDx12.hpp
+++ library/includes/D3D/RendererDx12.hpp
@@ -9,109 +9,238 @@ class RendererDx12 final : public Renderer
 {
 public:
 	RendererDx12(
-		const char* appName,
+		[[maybe_unused]] const char* appName,
 		void* windowHandle, std::uint32_t width, std::uint32_t height, std::uint32_t bufferCount,
 		std::shared_ptr<ThreadPool>&& threadPool, RenderEngineType engineType
-	);
+	) : m_gaia{
+			windowHandle, width, height, bufferCount, std::move(threadPool), engineType
+		}
+	{}
 
-	void FinaliseInitialisation() override;
+	void FinaliseInitialisation() override
+	{
+		m_gaia.FinaliseInitialisation();
+	}
 
-	void Resize(std::uint32_t width, std::uint32_t height) override;
+	void Resize(std::uint32_t width, std::uint32_t height) override
+	{
+		m_gaia.Resize(width, height);
+	}
 
 	[[nodiscard]]
-	Extent GetCurrentRenderingExtent() const noexcept override;
+	Extent GetCurrentRenderingExtent() const noexcept override
+	{
+		auto [width, height] = m_gaia.GetCurrentRenderArea();
+
+		return Renderer::Extent{ .width = width, .height = height };
+	}
 
 	[[nodiscard]]
-	Extent GetFirstDisplayCoordinates() const override;
+	Extent GetFirstDisplayCoordinates() const override
+	{
+		auto [width, height] = m_gaia.GetFirstDisplayCoordinates();
 
-	void SetShaderPath(const wchar_t* path) override;
+		return Renderer::Extent{ .width = width, .height = height };
+	}
+
+	void SetShaderPath(const wchar_t* path) override
+	{
+		m_gaia.GetRenderEngine().SetShaderPath(path);
+	}
 
 	[[nodiscard]]
-	std::uint32_t AddGraphicsPipeline(const ExternalGraphicsPipeline& gfxPipeline) override;
+	std::uint32_t AddGraphicsPipeline(const ExternalGraphicsPipeline& gfxPipeline) override
+	{
+		return m_gaia.GetRenderEngine().AddGraphicsPipeline(gfxPipeline);
+	}
 
 	void ReconfigureModelPipelinesInBundle(
 		std::uint32_t modelBundleIndex, std::uint32_t decreasedModelsPipelineIndex,
 		std::uint32_t increasedModelsPipelineIndex
-	) override;
+	) override {
+		m_gaia.GetRenderEngine().ReconfigureModelPipelinesInBundle(
+			modelBundleIndex, decreasedModelsPipelineIndex, increasedModelsPipelineIndex
+		);
+	}
 
-	void RemoveGraphicsPipeline(std::uint32_t pipelineIndex) noexcept override;
-
-	[[nodiscard]]
-	size_t AddTexture(STexture&& texture) override;
-
-	void UnbindTexture(size_t textureIndex, std::uint32_t bindingIndex) override;
-
-	[[nodiscard]]
-	std::uint32_t BindTexture(size_t textureIndex) override;
-
-	void UnbindExternalTexture(std::uint32_t bindingIndex) override;
-
-	void RebindExternalTexture(size_t textureIndex, std::uint32_t bindingIndex) override;
+	void RemoveGraphicsPipeline(std::uint32_t pipelineIndex) noexcept override
+	{
+		m_gaia.GetRenderEngine().RemoveGraphicsPipeline(pipelineIndex);
+	}
 
 	[[nodiscard]]
-	std::uint32_t BindExternalTexture(size_t textureIndex) override;
+	size_t AddTexture(STexture&& texture) override
+	{
+		return m_gaia.GetRenderEngine().AddTexture(std::move(texture));
+	}
 
-	void RemoveTexture(size_t textureIndex) override;
-
-	[[nodiscard]]
-	std::uint32_t AddModelBundle(std::shared_ptr<ModelBundle>&& modelBundle) override;
-
-	void RemoveModelBundle(std::uint32_t bundleIndex) noexcept override;
-
-	[[nodiscard]]
-	std::uint32_t AddMeshBundle(std::unique_ptr<MeshBundleTemporary> meshBundle) override;
-
-	void RemoveMeshBundle(std::uint32_t bundleIndex) noexcept override;
+	void UnbindTexture(size_t textureIndex, std::uint32_t bindingIndex) override
+	{
+		m_gaia.GetRenderEngine().UnbindTexture(textureIndex, bindingIndex);
+	}
 
 	[[nodiscard]]
-	std::uint32_t AddCamera(std::shared_ptr<Camera>&& camera) noexcept override;
+	std::uint32_t BindTexture(size_t textureIndex) override
+	{
+		return m_gaia.GetRenderEngine().BindTexture(textureIndex);
+	}
 
-	void SetCamera(std::uint32_t index) noexcept override;
-	void RemoveCamera(std::uint32_t index) noexcept override;
+	void UnbindExternalTexture(std::uint32_t bindingIndex) override
+	{
+		m_gaia.GetRenderEngine().UnbindExternalTexture(bindingIndex);
+	}
 
-	void Render() override;
-	void WaitForGPUToFinish() override;
+	void RebindExternalTexture(size_t textureIndex, std::uint32_t bindingIndex) override
+	{
+		m_gaia.GetRenderEngine().RebindExternalTexture(textureIndex, bindingIndex);
+	}
+
+	[[nodiscard]]
+	std::uint32_t BindExternalTexture(size_t textureIndex) override
+	{
+		return m_gaia.GetRenderEngine().BindExternalTexture(textureIndex);
+	}
+
+	void RemoveTexture(size_t textureIndex) override
+	{
+		m_gaia.GetRenderEngine().RemoveTexture(textureIndex);
+	}
+
+	[[nodiscard]]
+	std::uint32_t AddModelBundle(std::shared_ptr<ModelBundle>&& modelBundle) override
+	{
+		return m_gaia.GetRenderEngine().AddModelBundle(std::move(modelBundle));
+	}
+
+	void RemoveModelBundle(std::uint32_t bundleIndex) noexcept override
+	{
+		m_gaia.GetRenderEngine().RemoveModelBundle(bundleIndex);
+	}
+
+	[[nodiscard]]
+	std::uint32_t AddMeshBundle(std::unique_ptr<MeshBundleTemporary> meshBundle) override
+	{
+		return m_gaia.GetRenderEngine().AddMeshBundle(std::move(meshBundle));
+	}
+
+	void RemoveMeshBundle(std::uint32_t bundleIndex) noexcept override
+	{
+		m_gaia.GetRenderEngine().RemoveMeshBundle(bundleIndex);
+	}
+
+	[[nodiscard]]
+	std::uint32_t AddCamera(std::shared_ptr<Camera>&& camera) noexcept override
+	{
+		return m_gaia.GetRenderEngine().AddCamera(std::move(camera));
+	}
+
+	void SetCamera(std::uint32_t index) noexcept override
+	{
+		m_gaia.GetRenderEngine().SetCamera(index);
+	}
+
+	void RemoveCamera(std::uint32_t index) noexcept override
+	{
+		m_gaia.GetRenderEngine().RemoveCamera(index);
+	}
+
+	void Render() override { m_gaia.Render(); }
+
+	void WaitForGPUToFinish() override
+	{
+		m_gaia.GetRenderEngine().WaitForGPUToFinish();
+	}
 
 public:
 	// External stuff
 	[[nodiscard]]
-	ExternalResourceManager* GetExternalResourceManager() noexcept override;
+	ExternalResourceManager* GetExternalResourceManager() noexcept override
+	{
+		return m_gaia.GetRenderEngine().GetExternalResourceManager();
+	}
 
-	void UpdateExternalBufferDescriptor(const ExternalBufferBindingDetails& bindingDetails) override;
+	void UpdateExternalBufferDescriptor(
+		const ExternalBufferBindingDetails& bindingDetails
+	) override {
+		m_gaia.GetRenderEngine().UpdateExternalBufferDescriptor(bindingDetails);
+	}
 
 	void UploadExternalBufferGPUOnlyData(
-		std::uint32_t externalBufferIndex, std::shared_ptr<void> cpuData, size_t srcDataSizeInBytes,
-		size_t dstBufferOffset
-	) override;
+		std::uint32_t externalBufferIndex, std::shared_ptr<void> cpuData,
+		size_t srcDataSizeInBytes, size_t dstBufferOffset
+	) override {
+		m_gaia.GetRenderEngine().UploadExternalBufferGPUOnlyData(
+			externalBufferIndex, std::move(cpuData), srcDataSizeInBytes, dstBufferOffset
+		);
+	}
+
 	void QueueExternalBufferGPUCopy(
 		std::uint32_t externalBufferSrcIndex, std::uint32_t externalBufferDstIndex,
 		size_t dstBufferOffset, size_t srcBufferOffset = 0, size_t srcDataSizeInBytes = 0
-	) override;
+	) override {
+		m_gaia.GetRenderEngine().QueueExternalBufferGPUCopy(
+			externalBufferSrcIndex, externalBufferDstIndex, dstBufferOffset, srcBufferOffset,
+			srcDataSizeInBytes
+		);
+	}
 
 	[[nodiscard]]
-	std::uint32_t AddExternalRenderPass() override;
+	std::uint32_t AddExternalRenderPass() override
+	{
+		return m_gaia.AddExternalRenderPass();
+	}
+
 	[[nodiscard]]
-	ExternalRenderPass* GetExternalRenderPassRP(size_t index) const noexcept override;
+	ExternalRenderPass* GetExternalRenderPassRP(size_t index) const noexcept override
+	{
+		return m_gaia.GetRenderEngine().GetExternalRenderPassRP(index);
+	}
+
 	[[nodiscard]]
 	std::shared_ptr<ExternalRenderPass> GetExternalRenderPassSP(
 		size_t index
-	) const noexcept override;
+	) const noexcept override {
+		return m_gaia.GetRenderEngine().GetExternalRenderPassSP(index);
+	}
 
-	void SetSwapchainExternalRenderPass() override;
-
-	[[nodiscard]]
-	ExternalRenderPass* GetSwapchainExternalRenderPassRP() const noexcept override;
-	[[nodiscard]]
-	std::shared_ptr<ExternalRenderPass> GetSwapchainExternalRenderPassSP() const noexcept override;
-
-	void RemoveExternalRenderPass(size_t index) noexcept override;
-	void RemoveSwapchainExternalRenderPass() noexcept override;
+	void SetSwapchainExternalRenderPass() override
+	{
+		m_gaia.SetSwapchainExternalRenderPass();
+	}
 
 	[[nodiscard]]
-	size_t GetActiveRenderPassCount() const noexcept override;
+	ExternalRenderPass* GetSwapchainExternalRenderPassRP() const noexcept override
+	{
+		return m_gaia.GetRenderEngine().GetSwapchainExternalRenderPassRP();
+	}
 
 	[[nodiscard]]
-	ExternalFormat GetSwapchainFormat() const noexcept override;
+	std::shared_ptr<ExternalRenderPass> GetSwapchainExternalRenderPassSP() const noexcept override
+	{
+		return m_gaia.GetRenderEngine().GetSwapchainExternalRenderPassSP();
+	}
+
+	void RemoveExternalRenderPass(size_t index) noexcept override
+	{
+		m_gaia.GetRenderEngine().RemoveExternalRenderPass(index);
+	}
+
+	void RemoveSwapchainExternalRenderPass() noexcept override
+	{
+		m_gaia.GetRenderEngine().RemoveSwapchainExternalRenderPass();
+	}
+
+	[[nodiscard]]
+	size_t GetActiveRenderPassCount() const noexcept override
+	{
+		return m_gaia.GetRenderEngine().GetActiveRenderPassCount();
+	}
+
+	[[nodiscard]]
+	ExternalFormat GetSwapchainFormat() const noexcept override
+	{
+		return m_gaia.GetSwapchainFormat();
+	}
 
 private:
 	Gaia m_gaia;
